7a10b343c492498c532c5003727c32a66f840ee3
André Luferat <contato@luferat.net>
2024-11-01 16:33:30 -0300

    Ajustes

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Olá Mundo!</p>"
+    
+    page = {
+        'title': 'CRUDTrecos'
+    }
+    
+    return render_template('_template.html', **page)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
