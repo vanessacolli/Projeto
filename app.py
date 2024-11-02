4b4aa5dddb491e420adb27deb35829a3e6c339ba
André Luferat <contato@luferat.net>
2024-11-02 16:30:16 -0300

    template

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
+# Importa as dependências do aplicativo
 from flask import Flask, render_template
 
+# Cria um aplicativo Flask chamado "app"
 app = Flask(__name__)
 
-
+# Rota raiz, equivalente a páginai inicial do site (index)
 @app.route("/")
-def hello_world():
-    
-    page = {
-        'title': 'CRUDTrecos'
+def home(): # Função executada ao acessar a rota raiz
+
+    # Cria um usuário "fake" para testes
+    # No futuro, isso virá de um cookie
+    usuario = {
+        'nome': 'Joca da Silva',
+        'id': '1'
     }
     
-    return render_template('_template.html', **page)
+    # Extrai apenas o primeiro nome do usuário
+    usuario['pnome'] = usuario['nome'].split()[0]
 
+    # Dados, variáveis e valores a serem passados para o template HTML
+    pagina = {
+        'title': 'CRUDTrecos',
+        'user': usuario
+    }
 
+    # Renderiza o template HTML, passaod valores para ele
+    return render_template('_template.html', **pagina)
+
+# Executa o servidor HTTP se estiver no modo de desenvolvimento
+# Remova / comente essas linhas no modo de produção
 if __name__ == '__main__':
     app.run(debug=True)
