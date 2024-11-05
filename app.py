806789280661368ea88ea1fdec620ad4ff508bfd
André Luferat <contato@luferat.net>
2024-11-05 11:47:26 -0300

    dsndskfldk

--- app.py
+++ app.py
@@ -1,46 +1,21 @@
 # Importa as dependências do aplicativo
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Cria um aplicativo Flask chamado "app"
 app = Flask(__name__)
 
-# Rota raiz, equivalente a páginai inicial do site (index)
-@app.route("/")
-def home(): # Função executada ao acessar a rota raiz
-
-    # Cria um usuário "fake" para testes
-    # No futuro, isso virá de um cookie
-    usuario = {
-        'nome': 'Joca da Silva',
-        'id': '1'
-    }
-    
-    # Extrai apenas o primeiro nome do usuário
-    usuario['pnome'] = usuario['nome'].split()[0]
-
-    # Dados, variáveis e valores a serem passados para o template HTML
-    pagina = {
-        'titulo': 'CRUDTrecos',
-        'usuario': usuario
-    }
-
-    # Renderiza o template HTML, passaod valores para ele
-    return render_template('home.html', **pagina)
+# Cria um usuário "fake" para testes
+# No futuro, isso virá de um cookie
+usuario = {
+    'nome': 'Joca da Silva',
+    'id': '1'
+}
+# Extrai apenas o primeiro nome do usuário
+usuario['pnome'] = usuario['nome'].split()[0]
 
 
-
-@app.route('/novo')
-def novo():
-
-    # Cria um usuário "fake" para testes
-    # No futuro, isso virá de um cookie
-    usuario = {
-        'nome': 'Joca da Silva',
-        'id': '1'
-    }
-    
-    # Extrai apenas o primeiro nome do usuário
-    usuario['pnome'] = usuario['nome'].split()[0]
+@app.route("/")  # Rota raiz, equivalente a página inicial do site (index)
+def index():  # Função executada ao acessar a rota raiz
 
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
@@ -48,9 +23,58 @@ def novo():
         'usuario': usuario
     }
 
-    # Renderiza o template HTML, passaod valores para ele
-    return render_template('home.html', **pagina)
+    # Renderiza o template HTML, passando valores (pagina) para ele
+    return render_template('index.html', **pagina)
 
+
+# Rota para a página de cadastro de novo treco
+@app.route('/novo', methods=['GET', 'POST'])
+def novo():  # Função executada para cadastrar novo treco
+
+    # Se o formulário foi enviado
+    if request.method == 'POST':
+
+        # Obtém os dados preenchidos na forma de dicionário
+        form = dict(request.form)
+
+        # Teste de mesa (comente depois dos testes)
+        # Verifica se os dados do formulário chegaram ao back-end
+        print('\n\n\n', form, '\n\n\n')
+
+        # Em breve: grava os dados no banco de dados e segue o fluxo
+
+    # Dados, variáveis e valores a serem passados para o template HTML
+    pagina = {
+        'titulo': 'CRUDTrecos - Novo Treco',
+        'usuario': usuario
+    }
+
+    # Renderiza o template HTML, passaod valores para ele
+    return render_template('novo.html', **pagina)
+
+
+@app.route('/login', methods=['GET', 'POST'])  # Rota para login de usuário
+def login():
+
+    # Dados, variáveis e valores a serem passados para o template HTML
+    pagina = {
+        'titulo': 'CRUDTrecos - Login',
+        'usuario': usuario
+    }
+
+    return render_template('login.html', **pagina)
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+
+    # Dados, variáveis e valores a serem passados para o template HTML
+    pagina = {
+        'titulo': 'CRUDTrecos - Login',
+        'usuario': usuario
+    }
+
+    return render_template('cadastro.html', **pagina)
 
 
 # Executa o servidor HTTP se estiver no modo de desenvolvimento
